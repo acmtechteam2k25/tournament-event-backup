2ab83c9e3a2fe3c809698bce16a5795508c8fefd
TechTeam2024 <acmtechteam24@gmail.com>
2025-10-03 12:02:58 +0530

    font styles updated

--- src/components/AboutACM.jsx
+++ src/components/AboutACM.jsx
@@ -5,7 +5,7 @@ const AboutACM = () => {
     <section className="py-16 px-6 sm:px-8 md:px-4">
       <div className="max-w-4xl mx-auto">
         <div className="text-center mb-8 sm:mb-12">
-          <h2 className="bodoni-moda text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4 sm:mb-6">
+          <h2 className="dm-serif-display-regular text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4 sm:mb-6">
             About ACM VNRVJIET
           </h2>
           <div className="relative w-20 h-1 mx-auto mb-8">
@@ -20,7 +20,7 @@ const AboutACM = () => {
 
         <div className="max-w-3xl mx-auto text-center px-2 sm:px-4">
           <div className="prose prose-lg prose-invert mx-auto">
-            <p className="text-white/80 cal-sans-regular text-sm sm:text-base md:text-lg leading-relaxed mb-4 sm:mb-6 text-justify">
+            <p className="text-white/80 cal-sans-regular text-md lg:text-2xl sm:text-base md:text-lg leading-relaxed mb-4 sm:mb-6 text-justify">
               Established on October 24th, 2018, at VNRVJIET, Hyderabad, India, ACM VNRVJIET Student Chapter is a vibrant community with over 1200+ local membership holders. We've conducted more than 65 events, including national-level coding contests and hackathons. ACM bridges technology enthusiasts, educators, and professionals, offering a dynamic platform to explore the ever-evolving world of technology.
             </p>
             
--- src/components/CountSection.jsx
+++ src/components/CountSection.jsx
@@ -6,7 +6,7 @@ const CountSection = () => {
     <section className="relative py-16 px-4 bg-black/10 backdrop-blur-sm">
       <div className="max-w-6xl mx-auto">
         <div className="text-center mb-12">
-          <h2 className="bodoni-moda text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4">
+          <h2 className="dm-serif-display-regular text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4">
             Tournament Statistics
           </h2>
           <p className="cal-sans-regular text-white/80 text-lg max-w-2xl mx-auto">
