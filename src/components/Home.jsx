2dfeebe10ad5523d728b4d5f053bee6fcaa37284
TechTeam2024 <acmtechteam24@gmail.com>
2025-10-24 18:32:43 +0530

    title updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,8 +34,8 @@ const Home = () => {
 
         {/* Hero Content over the orb */}
         <main className="relative z-10 text-center px-4 -translate-y-5 sm:-translate-y-1">
-          <h1 className="tea-chest-regular text-5xl sm:text-7xl md:text-8xl lg:text-[6rem] xl:text-[7rem] font-bold text-white mb-0 sm:mb-8 leading-tight">
-            Tesseract
+          <h1 className="tea-chest-regular text-4xl sm:text-6xl md:text-7xl lg:text-[5rem] xl:text-[6rem] font-bold text-white mb-0 sm:mb-8 leading-tight">
+            Tech Tournament
           </h1>
           <h4 className="cal-sans-regular text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-white mb-2 sm:mb-8 leading-tight">
             The Ultimate Showdown
