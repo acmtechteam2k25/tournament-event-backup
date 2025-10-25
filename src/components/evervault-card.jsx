df6466ea70a92300a948c40e5a7d6e6049904988
TechTeam2024 <acmtechteam24@gmail.com>
2025-10-25 23:04:59 +0530

    Set min height of paragraph to full screen
    Updated the paragraph element to have a minimum height of the screen.

--- src/components/evervault-card.jsx
+++ src/components/evervault-card.jsx
@@ -65,7 +65,8 @@ export function CardPattern({ mouseX, mouseY, randomString }) {
         className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay  group-hover/card:opacity-100"
         style={style}
       >
-        <p className="absolute inset-x-0 text-xs h-full break-words whitespace-pre-wrap text-white font-mono font-bold transition duration-500">
+        <p className="absolute inset-x-0 text-xs min-h-screen break-words whitespace-pre-wrap text-white font-mono font-bold transition duration-500">
+          {randomString}
         </p>
       </motion.div>
     </div>
