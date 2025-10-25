818c28cbd71c778a803a2b8e7793c592a47d9d2e
TechTeam2024 <acmtechteam24@gmail.com>
2025-10-25 20:44:43 +0530

    poster updated

--- src/components/PosterSection.jsx
+++ src/components/PosterSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import eventPoster from "../assets/poster.jpg";
+import eventPoster from "../assets/poster.png";
 import { Calendar, MapPin } from "lucide-react";
 import "./PosterSection.css"; // make sure path matches file name
 
